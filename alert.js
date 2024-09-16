acff41b0ec7d8ee6464a8b8ff66ff10c7f323abe
theFoxCost <148291165+theFoxCost@users.noreply.github.com>
2024-09-16 22:53:31 +0200

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Get references to the elements
+    const card = document.getElementById('card');
+
+    // Add click event listener to the card
+    card.addEventListener('click', () => {
+        alert('This Content is Unvailable try Later !!!');
+    });
+
+    // // Add click event listener to the close button
+    // closeAlertButton.addEventListener('click', () => {
+    //     customAlert.classList.add('alert-hidden');
+    // });
+});
